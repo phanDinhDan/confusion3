0a1f4272b045b0bce147d226178e8e4bc46600f5
Dan <phandinhdan6602@gmail.com>
2023-06-14 03:37:41 +0700

    Function Components

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Main from "./components/MainComponent";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/components/Helloclasscomponent.js
@@ -0,0 +1,8 @@
+import React, {Component} from "react"
+class Hello extends Component{
+    render(){
+        return<h1> Class Component
+        </h1>
+    }
+}
+export default Hello
